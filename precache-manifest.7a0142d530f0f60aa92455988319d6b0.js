d3ac85af61c5acda910e66f7f257e1dbad2d9d1a
AxelHenriot <axlht@hotmail.com>
2019-04-27 14:17:45 -0300

    Updates

---
+++ precache-manifest.7a0142d530f0f60aa92455988319d6b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca603f24b52dd45d78b2",
+    "url": "/react_clientpanel/static/js/runtime~main.32c9d409.js"
+  },
+  {
+    "revision": "1642ac568a36f075cf5d",
+    "url": "/react_clientpanel/static/js/main.954f2731.chunk.js"
+  },
+  {
+    "revision": "7ff8111948281addb7d7",
+    "url": "/react_clientpanel/static/js/2.e65bac3d.chunk.js"
+  },
+  {
+    "revision": "1642ac568a36f075cf5d",
+    "url": "/react_clientpanel/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "34941bfcd368c93a37deca87af2cb075",
+    "url": "/react_clientpanel/index.html"
+  }
+];
